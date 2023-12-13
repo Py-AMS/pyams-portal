c30e00fec42cbdbd9abde4eb855ef1830eb5fa6e
Thierry Florac <thierry.florac@onf.fr>
2023-12-13 09:24:54 +0100

    Added Gulp minify task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const { src, dest, task, watch, parallel } = require('gulp');
 
 const clean = require('gulp-clean-css');
 const rename = require('gulp-rename');
+const minify = require('gulp-minify');
 const sass = require('gulp-sass')(require('node-sass'));
 
 
@@ -20,12 +21,24 @@ task('sass', function() {
         .pipe(dest('src/pyams_portal/zmi/resources/css/'));
 });
 
+task('js', function() {
+    return src('src/pyams_portal/zmi/resources/js/layout.js')
+        .pipe(minify({
+            ext: {
+                min: '.min.js'
+            }
+        }))
+        .pipe(dest('src/pyams_portal/zmi/resources/js/'));
+})
 
 exports.sass_dev = task('sass_dev');
 exports.sass_prod = task('sass');
+exports.minify_js = task('js');
 
 
 exports.default = function() {
-	watch('src/pyams_portal/zmi/resources/sass/*.scss',
-		parallel('sass_dev', 'sass'));
+    watch('src/pyams_portal/zmi/resources/sass/*.scss',
+        parallel('sass_dev', 'sass'));
+    watch('src/pyams_portal/zmi/resources/js/layout.js',
+        parallel('js'));
 };
